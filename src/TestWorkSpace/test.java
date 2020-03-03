0c499ebbf5dec0091b56c37cf479c4ef80fc9358
douzihedouyacai <zhh_fu@163.com>
2020-03-03 19:13:38 +0800

    更新剑指Offer题解 12 计算某个数的幂值 13 调整数组使奇数在前偶数在后

--- src/TestWorkSpace/test.java
+++ src/TestWorkSpace/test.java
@@ -1,11 +1,35 @@
 package TestWorkSpace;
 
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args){
-        StringBuffer str = new StringBuffer("We Are Happy");
-        System.out.println(str.charAt(0));
-        System.out.println(str.replace(2,3,"%20"));
-        System.out.println("".length());
-        System.out.println(" ".length());
+        int[] arr = {2,5,3,7,5,1,9,4};
+        insertSort(arr);
+        for (int i:arr) {
+            System.out.println(i);
+        }
+    }
+
+    private static void insertSort(int[] arr){
+        if (arr == null || arr.length < 2){
+            return;
+        }
+        for (int i = 1;i<arr.length;i++){
+            int index = i;
+            for (int j = i-1;j>=0;j--){
+                if (arr[j] > arr[index]){
+                    swap(arr,j,index);
+                    index--;
+                }
+            }
+        }
+        //return arr;
+    }
+
+    private static void swap(int[] arr,int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
